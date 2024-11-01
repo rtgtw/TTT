7ac68c72a4319fc1fee12c56e8df01184bd645ce
Remi Olateru <remiolateru@gmail.com>
2024-11-01 18:07:15 -0400

    creating player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,32 @@
-export default function Player({name, symbol}){
+import {useState} from 'react';
 
+export default function Player({name, symbol}){
     //Manage data that will cause the UI to update when changed
+    //use state method
+
+    //You can pass an initial value to useState's parameters
+    //then you use destructoring to create variables of the array returned
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick(){
+        setIsEditing( (editing) => !editing);
+    }
+
+    let playerName = <span className="player-name">{name}</span>;
+    let buttonCaption = 'Edit';
+
+    if(isEditing){
+        playerName = <input type="text" required value={name}></input>
+        buttonCaption = 'Save';
+    }
+   
     return <>
          <li>
             <span className="player">
-            <span className="player-name">{name}</span>
-            <span className="player-symbol">{symbol}</span>
+           {playerName}
+            <span className="player-symbol">{symbol}</span> 
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>{buttonCaption}</button>
         </li>
     
     
