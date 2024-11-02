8241aacf546559329d1ea403f0c17e8f46760449
Remi Olateru <remiolateru@gmail.com>
2024-11-02 14:51:48 -0400

    two way binding with Player component, to get input and pass it back

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 
-export default function Player({name, symbol}){
+export default function Player({initialName, symbol}){
     //Manage data that will cause the UI to update when changed
     //use state method
 
@@ -8,22 +8,29 @@ export default function Player({name, symbol}){
     //then you use destructoring to create variables of the array returned
     const [isEditing, setIsEditing] = useState(false);
 
+    const [playerName, setPlayerName] = useState(initialName);
+
     function handleEditClick(){
         setIsEditing( (editing) => !editing);
     }
 
-    let playerName = <span className="player-name">{name}</span>;
+    function handleChange(event){
+        console.log(event);
+            setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
     let buttonCaption = 'Edit';
 
     if(isEditing){
-        playerName = <input type="text" required value={name}></input>
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}></input>
         buttonCaption = 'Save';
     }
    
     return <>
          <li>
             <span className="player">
-           {playerName}
+           {editablePlayerName}
             <span className="player-symbol">{symbol}</span> 
             </span>
             <button onClick={handleEditClick}>{buttonCaption}</button>
