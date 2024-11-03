5c28e92b5cffcf751611b6afaa8423dd64d789d0
Remi Olateru <remiolateru@gmail.com>
2024-11-02 20:25:24 -0400

    creating gameboard logic using the spread operator and a deep copy

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,6 @@
+import {useState} from 'react';
+
+
 const initialGameBoard = 
  [
 [null, null,null],
@@ -7,15 +10,32 @@ const initialGameBoard =
 
 
 export default function GameBoard(){
+    //we are using this state to manage and update the gameboard
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+
+    //create on click function when button is clicked
+    //prevgameBoard is automatically passed into setGameBoard, now we can use it to update the gameboard
+    function handleSelectSquare(rowIndex, colIndex){
+        setGameBoard( (prevGameBoard)=> {
+            //replace one field with an X or an O
+
+            const updatedBoard = [...prevGameBoard.map((innerArray) =>  [...innerArray]  )];
+            updatedBoard[rowIndex][colIndex] = 'X';
+            return updatedBoard;
+        } );
+    }
+
 
     //.map arrow function will be called for each element within initalGameboard
     //in this case thats the three arrays, the parameter of .map would be the element(array)
     //that we receive in order to transform it in some way
     return <>
     <ol id="game-board">
-        {initialGameBoard.map( (row, rowIndex) => <li key={rowIndex}>
+        {gameBoard.map( (row, rowIndex) => <li key={rowIndex}>
             <ol>
-                {row.map( (playerSymbol, colIndex) => <li key={colIndex}> <button>{playerSymbol}</button> </li>)}
+                {row.map( (playerSymbol, colIndex) => 
+                <li key={colIndex}> <button onClick={() => handleSelectSquare(rowIndex,colIndex) }>{playerSymbol}</button> </li>)}
             </ol>
         </li> )}
     </ol>
